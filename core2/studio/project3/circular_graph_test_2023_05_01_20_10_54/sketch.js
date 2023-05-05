bd3180f5cb2c74246c366faadea062cc7721b2d9
ann <123750949+annnabui@users.noreply.github.com>
2023-05-05 10:04:14 -0400

    map complete

--- core2/studio/project3/circular_graph_test_2023_05_01_20_10_54/sketch.js
+++ core2/studio/project3/circular_graph_test_2023_05_01_20_10_54/sketch.js
@@ -1,76 +1,151 @@
 var canvas;
-var strokeOne;
-var strokeOut;
-let strokeOutID = "strokeout";
+let data, url;
+let myFont;
+
+function preload() {
+  url = 'oyster.json';
+  data = loadJSON(url);
+  
+  myFont = loadFont('Helvetica-Bold.ttf');
+}
+
+let seg1 = 0
+let seg2 = 0
+let seg3 = 0
+let seg4 = 0
+let seg5 = 0
+let seg6 = 0
+let seg7 = 0
+let seg8 = 0
+
 
 function setup() {
+  angleMode(DEGREES);
   canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+  console.log(data);
   normalMaterial();
   noDebugMode();
+  noFill();
+  stroke(68,74,246,120);
+  strokeWeight(150);
 
   canvas.position(0,0);
   canvas.style('z-index', '-1');
   strokeCap(SQUARE);
+  let dataLength = Object.keys(data).length
+  for (let i=0; i < dataLength; i++) {
+    let currentEntry = data[i];
+    let currentEntryYear = currentEntry['first_appeared'];
+    if (currentEntryYear <= 1859 && currentEntryYear >= 1851) {
+      seg1 += currentEntry['menus_appeared']
+      
+    } else if(currentEntryYear <= 1880 && currentEntryYear >= 1860) {
+      seg2 += currentEntry['menus_appeared']
 
-  strokeOne = function() {
-    stroke(68,74,246,120);
-    strokeWeight(150);
-  }
+    } else if(currentEntryYear <= 1901 && currentEntryYear >= 1881) {
+      seg3 += currentEntry['menus_appeared']
 
-  strokeOut = function() {
-    stroke(68,74,246,120);
-    strokeWeight(150);
-  }
-
-  // let strokeOutObj = {var: strokeOut, id: strokeOutID};
-
-  // console.log(strokeOutObj.var);
-  // console.log(strokeOutObj.id);
+    } else if(currentEntryYear <= 1922 && currentEntryYear >= 1902) {
+      seg4 += currentEntry['menus_appeared']
+      
+    } else if(currentEntryYear <= 1943 && currentEntryYear >= 1923) {
+      seg5 += currentEntry['menus_appeared']
+      
+    } else if(currentEntryYear <= 1964 && currentEntryYear >= 1944) {
+      seg6 += currentEntry['menus_appeared']
+      
+    } else if(currentEntryYear <= 1985 && currentEntryYear >= 1965) {
+      seg7 += currentEntry['menus_appeared'] 
+      
+    } else if(currentEntryYear <= 2006 && currentEntryYear >= 1986) {
+      seg8 += currentEntry['menus_appeared'] 
+    }
+  }  
+  
+  console.log(seg1,seg2,seg3,seg4,seg5,seg6,seg7,seg8)
 }
 
 function draw() {
   background(0,10,255);
+  //pieChart(300, menuNo);
   // background(000)
+
   orbitControl();
-
+  let scaledSeg1 = map(seg1, 2, 4160, 60, 360);
+  let scaledSeg2 = map(seg2, 2, 4160, 60, 360);
+  let scaledSeg3 = map(seg3, 2, 4160, 60, 360);
+  let scaledSeg4 = map(seg4, 2, 4160, 60, 360);
+  let scaledSeg5 = map(seg5, 2, 4160, 60, 360);
+  let scaledSeg6 = map(seg6, 2, 4160, 60, 360);
+  let scaledSeg7 = map(seg7, 2, 4160, 60, 360);
+  let scaledSeg8 = map(seg8, 2, 4160, 60, 360);
+  
+  fill(68,74,246,200);
+  textFont(myFont);
+  textSize(36);
+  text('1851 - 1859', -100, -250)
   noFill();
-  strokeOne ();
-  arc(0,-10,300,300,PI*3/4, PI*5/4);
-  translate(0,0,150);
+  arc(0,-10,300,300,135, 135 + scaledSeg1)
+  translate(0,0,200);
 
-
-  arc(0,-10,300,300,PI*5/3,PI*3/4);
-  arc(0,-10,300,300,PI*7/6,PI*3/2);
-  translate(0,0,150);
-
-  arc(0,-10,300,300,0,PI/4);
-  arc(0,-10,300,300,PI,PI*3/2);
+  fill(68,74,246,200);
+  textFont(myFont);
+  textSize(36);
+  text('1860 - 1880', -100, -250)
+  noFill();
+  arc(0,-10,300,300,300,300 + scaledSeg2);
+  translate(0,0,200);
   
-  arc(0,-10,300,300,0,PI/2);
-  translate(0,0,150);
+  fill(68,74,246,200);
+  textFont(myFont);
+  textSize(36);
+  text('1881 - 1901', -100, -250)
+  noFill();
+  arc(0,-10,300,300,210,210 + scaledSeg3);
+  translate(0,0,200);
+
+  fill(68,74,246,200);
+  textFont(myFont);
+  textSize(36);
+  text('1902 - 1922', -100, -250)
+  noFill();
+  arc(0,-10,300,300,0, 0 + scaledSeg4);
+  translate(0,0,200);
   
-  arc(0,-10,300,300,0,PI);
-  translate(0,0,150);
-
-  arc(0,-10,300,300,PI*3/4,PI*3/2);
-  translate(0,0,0);
-
-  arc(0,-10,300,300,PI*5/3,PI*7/4);
-  translate(0,0,0);
-
-  arc(0,-10,300,300,0,PI*1/4);
-  translate(0,0,150);
+  fill(68,74,246,200);
+  textFont(myFont);
+  textSize(36);
+  text('1923 - 1943', -100, -250)
+  noFill();
+  arc(0,-10,300,300,275, 275 + scaledSeg5);
+  translate(0,0,200);
   
+  fill(68,74,246,200);
+  textFont(myFont);
+  textSize(36);
+  text('1944 - 1964', -100, -250)
+  noFill();
+  arc(0,-10,300,300,200, 200 + scaledSeg6);
+  translate(0,0,200);
 
-  strokeOut();
-  arc(0,-10,300,300,PI/2,PI*5/4);
-  translate(0,0,150);
-};
+  fill(68,74,246,200);
+  textFont(myFont);
+  textSize(36);
+  text('1965 - 1985', -100, -250)
+  noFill();
+  arc(0,-10,300,300,300,300 + scaledSeg7);
+  translate(0,0,200);
 
-// var strokeClick = document.getElementById('strokeout');
-// var strokePopup = document.getElementById('myDiv');
+  fill(68,74,246,200);
+  textFont(myFont);
+  textSize(36);
+  text('1986 - 2006', -100, -250)
+  noFill();
+  arc(0,-10,300,300,300,300 + scaledSeg8);
+  translate(0,0,200);
 
-// strokeClick.addEventListener("click", function() {
-//   strokePopup.style.display = "block";
-// });
+}
 
+function windowResized () {
+  resizeCanvas(windowWidth, windowHeight);
+}
