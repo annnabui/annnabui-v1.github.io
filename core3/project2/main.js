59052c62783c260a664a71a91aa96367c0f2359e
ann <123750949+annnabui@users.noreply.github.com>
2023-10-09 20:07:31 -0400

    carry on 1st commit

---
+++ core3/project2/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    var header = $("#header");
+    var navigation = $(".navigation");
+    // Initially hide the navigation
+    navigation.hide();
+
+    $(window).on("scroll", function () {
+        console.log("Scroll event triggered");
+        var headerTop = header.offset().top;
+        var headerBottom = headerTop + header.height();
+        var viewportTop = $(window).scrollTop();
+
+        if (headerBottom <= viewportTop) {
+            navigation.show();
+        } else {
+            navigation.hide();
+        }
+        });
+    });
